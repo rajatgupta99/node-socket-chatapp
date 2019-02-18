debc921ea22c3fb18fb2f448556e24762e546d25
Rajat Gupta Thinkpad X1 <rajatgupta99@gmail.com>
2019-02-17 17:17:47 -0800

    v1

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 var app = express();
 var publicPath = path.join(__dirname, '..', '/public');
 var PORT = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () =>  {
+io.on('connect', (socket) =>  {
+  console.log("New User Connected!");
+
+  socket.on('disconnect', ()  =>  {
+    console.log('USER DISCONNECTED');
+  })
+});
+
+
+server.listen(PORT, () =>  {
     console.log(`Listening on PORT: ${PORT}`);
-})
+});
